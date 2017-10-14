2f58b0d5a7c97d07de8fccf588005444c91df98a
Pavel Viznitsa <viznitsap@gmail.com>
2017-10-14 15:55:42 +0300

    add advertising-text

--- main.js
+++ main.js
@@ -7,17 +7,21 @@ document.onmouseover = function(event) {
     }
 }
 
+var adv_text = 'ADVERTISING. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, ad, cum. Incidunt harum mollitia assumenda, maxime, unde quaerat voluptatibus. Illo temporibus aut quasi blanditiis eaque, officia quibusdam pariatur porro doloribus.';
+
 function showAdv(a) {
-    console.log(a);
-    console.log(a.target.className);
     if (a.offsetX > 295) {
         document.querySelector('.right').style.right = 0;
+        document.querySelector('.right').innerHTML = adv_text;
     } else if (a.offsetX < 5) {
         document.querySelector('.left').style.left = 0;
+        document.querySelector('.left').innerHTML = adv_text;
     } else if (a.offsetY < 5) {
         document.querySelector('.top').style.top = 0;
+        document.querySelector('.top').innerHTML = adv_text;
     } else if (a.offsetY > 295) {
         document.querySelector('.bot').style.bottom = 0;
+        document.querySelector('.bot').innerHTML = adv_text;
     }
 }
 
