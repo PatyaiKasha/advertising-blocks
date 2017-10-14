7f9796141c3d1023d905ffa9d5b5d139f7f51131
Pavel Viznitsa <viznitsap@gmail.com>
2017-10-14 15:25:47 +0300

    almost made

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 document.onmouseover = function(event) {
-    // console.log(e);
-    if (event.target.className == 'mainAdv') {
+    var et = event.target;
+    if (et.className == 'mainAdv') {
         showAdv(event);
-    } else {
-        // hideAdv(event.target);
+    } else if (et.className != 'mainAdv' && et.className != 'top' && et.className != 'right' && et.className != 'bot' && et.className != 'left') {
+        hideAdv(event);
     }
 }
 
 function showAdv(a) {
     console.log(a);
-    console.log('mainAdv-block');
+    console.log(a.target.className);
     if (a.offsetX > 295) {
         document.querySelector('.right').style.right = 0;
     } else if (a.offsetX < 5) {
@@ -22,15 +22,8 @@ function showAdv(a) {
 }
 
 function hideAdv(b) {
-    // console.log(b);
-    // if (b != document.querySelector('.mainAdv')) {
-    //     // onmouseover() = false;
-    //     document.querySelector('.right').style.right = '-300px';
-
-    //     document.querySelector('.left').style.left = '-300px';
-
-    //     document.querySelector('.top').style.top = '-300px';
-
-    //     document.querySelector('.bot').style.bottom = '-300px';
-    // }
+    document.querySelector('.right').style.right = '-300px';
+    document.querySelector('.left').style.left = '-300px';
+    document.querySelector('.top').style.top = '-300px';
+    document.querySelector('.bot').style.bottom = '-300px';
 }
